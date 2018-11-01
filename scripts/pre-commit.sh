70e1445aa846e303b221fe2e0dd379c35001731b
Filip Sodić <filip.sodic@fer.hr>
2018-11-01 17:39:43 +0100

    add tests to pre-commit

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,7 +3,12 @@ STASH_NAME="pre-commit-$(date +%s)"
 git stash save -q --keep-index $STASH_NAME
 
 # Test prospective commit
-pwd
+echo 'Running tests...'
+python3 -m unittest test_sammock.py
+if [ $? -ne 0 ]; then
+    echo "All tests need to pass in order to commit."
+    exit 1
+fi
 
 STASHES=$(git stash list)
 if [[ $STASHES == "$STASH_NAME" ]]; then
